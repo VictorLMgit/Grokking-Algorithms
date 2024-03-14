f1ba1cb40e2bd34e47a12194a2601a9030799177
Victor Lima <95509138+VictorLMgit@users.noreply.github.com>
2024-03-14 11:36:15 -0300

    Update quicksort.js

--- chapter4-divide-and-conquer/quicksort.js
+++ chapter4-divide-and-conquer/quicksort.js
@@ -6,6 +6,6 @@ function quickSort(arr) {
     if (arr.length < 2) return arr;
     const pivot = arr[0];
     const lessThanPivot = arr.slice(1).filter(n => n <= pivot);
-    const higherThanPivot = arr.slice(1).filter(n => (n > pivot) );
+    const higherThanPivot = arr.slice(1).filter(n => (n > pivot));
     return quickSort(lessThanPivot).concat(pivot).concat(quickSort(higherThanPivot));
 }
